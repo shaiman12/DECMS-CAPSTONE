787cb1b4435d10f005afa2603d501866fe7a1510
Avi Lurie <avilurie24@gmail.com>
2020-08-25 12:05:36 +0200

    Extractor now only saves the links to the css files

--- utils/HTML_Localizer.py
+++ utils/HTML_Localizer.py
@@ -12,11 +12,11 @@ class HTML_Localizer:
 
     """
     method traverses soup for for link tags and finds those css hrefs. Adds these links to an array and saves them to file
-    Need to test to see if I need to save the actaul file content for localizing. 
+    Need to test to see if I need to save the actual file content for localizing. 
     """
     def extract_css(self):
         css_files = []
-        for css in self.htmlSoup.find_all("link"):
+        for css in self.htmlSoup.find_all(type='text/css'):
             if css.attrs.get("href"):
                 css_url = urljoin(self.url, css.attrs.get("href"))
                 css_files.append(css_url)

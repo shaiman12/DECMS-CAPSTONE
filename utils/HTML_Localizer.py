79b9473aa67bb7e6fab55291ab38d66a22db5588
Shai Aarons <shai.aarons21@gmail.com>
2020-08-25 14:33:40 +0200

    Regular HTML images are downloading. Need to do javascript images

--- utils/HTML_Localizer.py
+++ utils/HTML_Localizer.py
@@ -1,27 +1,60 @@
 import requests
 from bs4 import BeautifulSoup as bSoup
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlparse
+import os
+from tqdm import tqdm
+import shutil
+from requests_html import HTMLSession
+
 
 class HTML_Localizer:
     """
-    For now, Localizer recieves the url and creates two variables with the seconf being the htmlSoup used for parsing data 
+    For now, Localizer recieves the url and creates two variables with the seconf being the htmlSoup used for parsing data
     """
+
     def __init__(self, url):
         self.url = url
-        self.htmlSoup = bSoup(requests.Session().get(url).content, "html.parser")
+        self.imgPath = "imgs"
+        self.htmlSoup = bSoup(
+            requests.Session().get(url).content, "html.parser")
 
     """
     method traverses soup for for link tags and finds those css hrefs. Adds these links to an array and saves them to file
-    Need to test to see if I need to save the actual file content for localizing. 
+    Need to test to see if I need to save the actual file content for localizing.
     """
+
     def extract_css(self):
         css_files = []
         for css in self.htmlSoup.find_all(type='text/css'):
             if css.attrs.get("href"):
                 css_url = urljoin(self.url, css.attrs.get("href"))
                 css_files.append(css_url)
-        
+
         with open("css_files.txt", "w") as f:
             for css_file in css_files:
                 print(css_file, file=f)
 
+    def get_image_list(self, url):
+        """Returns a list of all links of images from a URL"""
+        links = []
+        for image in self.htmlSoup.find_all("img"):
+            imageurl = image.attrs.get("src")
+            if not imageurl:
+                continue
+            imageurl = urljoin(url, imageurl)
+            try:
+                # removing "?" from imgs
+                x = imageurl.index("?")
+                imageurl = imageurl[:x]
+            except:
+                pass
+            links.append(imageurl)
+            return links
+
+    def download_img(self, image_url):
+        filename = image_url.split("/")[-1]
+        r = requests.get(image_url, stream=True)
+        if r.status_code == 200:
+            r.raw.decode_content = True
+            with open(filename, 'wb') as f:
+                shutil.copyfileobj(r.raw, f)
--- utils/web_scraper.py
+++ utils/web_scraper.py
@@ -1,6 +1,6 @@
 import requests
 from datetime import datetime
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urljoin
 from zipfile import ZipFile
 from utils.HTML_Localizer import HTML_Localizer
 
@@ -11,16 +11,22 @@ class web_scaper():
 
     def __init__(self, url):
         self.base_path = urlparse(url).hostname
+        self.imgPath = "imgs"
 
     def create_html_file(self, url):
         response = requests.get(url)
         #------------------------------------------------------------------------------------#
         localizeContent = HTML_Localizer(url)
         localizeContent.extract_css()
+        imagelist = localizeContent.get_image_list(url)
+        for img in imagelist:
+            localizeContent.download_img(img)
+
         #------------------------------------------------------------------------------------#
         now = datetime.now().strftime("%m/%d/%Y-%H:%M:%S").replace('/', '-')
         filename = self.base_path+'-'+now+".html"
         f = open(filename, "w")
         f.write(response.text)
         self.created_files.append(filename)
+
         return filename

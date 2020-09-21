d961df9412e790ff8547cc4a5a4a365215b06791
Shai Aarons <shai.aarons21@gmail.com>
2020-09-21 16:31:51 +0200

    Pushing for merge

--- utils/HTML_Localizer.py
+++ utils/HTML_Localizer.py
@@ -53,15 +53,24 @@ class HTML_Localizer:
             links.append(self.link_maker(image))
         return links
 
+
+# Returns a list of all links of videos from a URL
+
+
     def get_video_list(self):
         links = []
         print('Getting list of videos...')
 
-        for video in self.htmlSoup.find_all('source', type='video/mp4'):
+        for video in self.htmlSoup.find_all('source', type='video/ogg'):
             links.append(self.link_maker(video))
         for video in self.htmlSoup.find_all('source', type='video/mp4'):
+            links.append(self.link_maker(video))
+        for video in self.htmlSoup.find_all('source', type='video/webm'):
             links.append(self.link_maker(video))
         return links
+
+
+# Formats a link into correct form for downloading
 
     def link_maker(self, mediaItem):
         mediaurl = mediaItem.attrs.get("src")
@@ -74,6 +83,10 @@ class HTML_Localizer:
             pass
         return mediaurl
 
+
+# Returns a list of all links of videos from a URL
+
+
     def get_audio_list(self):
         links = []
         print('Getting list of videos...')
@@ -85,7 +98,7 @@ class HTML_Localizer:
 
         return links
 
-    """ Receives a list of image urls and downloads them locally  """
+# Receives a URL and downloads the file locally
 
     def download_media(self, media_url):
 
@@ -96,11 +109,15 @@ class HTML_Localizer:
             with open(filename, 'wb') as f:
                 shutil.copyfileobj(r.raw, f)
 
-    """ Using the html soup, this method replaces the old image url with the new locally saved version """
+
+# replaces all old image urls with the new locally saved versions
+
 
     def replaceImg(self):
         for image in self.htmlSoup.find_all("img"):
             self.replaceMedia(image)
+
+# Using the html soup, this method replaces the old media url with the new locally saved version
 
     def replaceMedia(self, media):
         downloadedMedia = os.listdir("media/")
@@ -113,34 +130,25 @@ class HTML_Localizer:
         if(pos > -1):
             media["src"] = "media/"+downloadedMedia[pos]
 
+
+# replaces all old video urls with the new locally saved versions
+
+
     def replaceVideos(self):
         for video in self.htmlSoup.find_all('source', type='video/mp4'):
             self.replaceMedia(video)
+        for video in self.htmlSoup.find_all('source', type='video/ogg'):
+            self.replaceMedia(video)
+        for video in self.htmlSoup.find_all('source', type='video/webm'):
+            self.replaceMedia(video)
+
+
+# replaces all old audio urls with the new locally saved versions
 
     def replaceAudio(self):
         downloadedMedia = os.listdir("media/")
 
         for audio in self.htmlSoup.find_all('source', type='audio/ogg'):
-
-            audioLink = audio.attrs.get("src")
-            if not audioLink:
-                continue
-            dissasembled = urlparse(audioLink)
-            filename, file_ext = os.path.splitext(
-                os.path.basename(dissasembled.path))
-            audioPart = filename+file_ext
-            pos = downloadedMedia.index(audioPart)
-            if(pos > -1):
-                audio["src"] = "media/"+downloadedMedia[pos]
+            self.replaceMedia(audio)
         for audio in self.htmlSoup.find_all('source', type='audio/mpeg'):
-
-            audioLink = audio.attrs.get("src")
-            if not audioLink:
-                continue
-            dissasembled = urlparse(audioLink)
-            filename, file_ext = os.path.splitext(
-                os.path.basename(dissasembled.path))
-            audioPart = filename+file_ext
-            pos = downloadedMedia.index(audioPart)
-            if(pos > -1):
-                audio["src"] = "media/"+downloadedMedia[pos]
+            self.replaceMedia(audio)

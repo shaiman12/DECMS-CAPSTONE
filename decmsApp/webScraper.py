6e53b0086d8a1330cfb8823053f53a8826ba2425
shai <shaiunterslak@gmail.com>
2020-10-15 17:52:19 +0200

    Fix single page download add some tests

--- decmsApp/webScraper.py
+++ decmsApp/webScraper.py
@@ -178,3 +178,4 @@ class webScraper():
             formattedUrl = 'http://'+formattedUrl
         return formattedUrl
 
+    
--- testLocalizer.py
+++ testLocalizer.py
@@ -9,23 +9,35 @@ import shutil
 class TestHTMLLocalizer(unittest.TestCase):
     # randomMedia=["https://woocommerce-343891-1062302.cloudwaysapps.com/wp-content/uploads/2019/12/logo-optimised-0-300x156.png","https://www.weare-creative.com/wp-content/uploads/2018/05/Show-reel-for-home-page-correct-size-edit.mp4","https://uploads-ssl.webflow.com/5e6fc6440ad9f713e1bda812/5e7b784b977c89ce45061ab7_path-179.svg","https://www.obama.org/wp-content/uploads/21-scaled-640x0-c-default.jpg","https://ssl.gstatic.com/dictionary/static/pronunciation/2019-10-21/audio/da/dado_en_us_1.mp3"]
 
-    # def test_downloadCSS(self):
-    #     url='https://www.reddbar.com/'
-    #     htmlSoup = bSoup(requests.Session().get(url, headers={'User-Agent': '...', 'referer': 'https://...'}).content, "html.parser")
-    #     localizer = htmlLocalizer(url,htmlSoup)
-    #     localizer.downloadCSS('test-css/testCSS_')
-    #     downloadedCSS= os.listdir("test-css/")
-    #     expectedNumberOfFiles=3
-    #     self.assertEqual(len(downloadedCSS),expectedNumberOfFiles)
-    #     shutil.rmtree("test-css/")
+    def test_getCorrectNumberOfLinks(self):
+        url='https://www.dadoagency.com/'
+        response = requests.Session().get(url, headers={'User-Agent': '...', 'referer': 'https://...'})
+        localizer = htmlLocalizer(url,response)
+        cssLinks=localizer.getAndReplaceCSS()
+        self.assertEqual(len(cssLinks),1)
+        bgLinks=localizer.getBgImageList()
+        self.assertEqual(len(bgLinks),0)
+        jsLinks=localizer.getAndReplaceJS()
+        self.assertEqual(len(jsLinks),3)
+        audioLinks=localizer.getAudioVideolist()
+        print(audioLinks)
+        self.assertEqual(len(audioLinks),1)
 
-    # def test_getImgList(self):
-    #     url='https://woocommerce-343891-1062302.cloudwaysapps.com/coming-soon/'
-    #     htmlSoup = bSoup(requests.Session().get(url, headers={'User-Agent': '...', 'referer': 'https://...'}).content, "html.parser")
-    #     localizer = htmlLocalizer(url,htmlSoup)
-    #     images=localizer.getImageList()
-    #     expectedNumberOfImages=2
-    #     self.assertEqual(len(images),expectedNumberOfImages)
+
+        url='https://wordpress-343891-1520087.cloudwaysapps.com/other'
+        response = requests.Session().get(url, headers={'User-Agent': '...', 'referer': 'https://...'})
+        localizer = htmlLocalizer(url,response)
+        cssLinks=localizer.getAndReplaceCSS()
+        self.assertEqual(len(cssLinks),3)
+        bgLinks=localizer.getBgImageList()
+        self.assertEqual(len(bgLinks),1)
+        jsLinks=localizer.getAndReplaceJS()
+        self.assertEqual(len(jsLinks),2)
+        audioLinks=localizer.getAudioVideolist()
+        self.assertEqual(len(audioLinks),0)
+
+
+
 
     # def test_downloadMedia(self):
     #     url='https://woocommerce-343891-1062302.cloudwaysapps.com/coming-soon/'
